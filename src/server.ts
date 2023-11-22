3bc582e49666d518d6944ab82ba7a700e58e10d5
Pervej0 <mdparvez222khan@gmail.com>
2023-11-23 01:34:22 +0600

    project basic code added

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./app/config/config";
+
+const main = async () => {
+  try {
+    await mongoose.connect(config.url as string);
+    app.listen(config.port, () => {
+      console.log("The server is running on port", config.port);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+main();
